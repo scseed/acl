8c3f728f4b8ee26e77ac366bc738171ac2e7700e
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-03-05 11:07:17 +0300

    scetches of assertion table structure

--- mysq.sql
+++ mysq.sql
@@ -1,60 +1,135 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT=0;
-START TRANSACTION;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE TABLE IF NOT EXISTS `acls` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `role_id` tinyint(3) unsigned NOT NULL,
-  `resource_id` smallint(5) unsigned NOT NULL,
-  `action_id` tinyint(3) unsigned NOT NULL,
-  `regulation` enum('allow','deny') NOT NULL DEFAULT 'allow',
-  PRIMARY KEY (`id`),
-  KEY `fk_acl_role` (`role_id`),
-  KEY `fk_acl_resource` (`resource_id`),
-  KEY `fk_acl_action` (`action_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE SCHEMA IF NOT EXISTS `russiankohana_kohanaworld_dev` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `russiankohana_kohanaworld_dev` ;
 
-CREATE TABLE IF NOT EXISTS `actions` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `resources` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_id` smallint(5) unsigned DEFAULT NULL,
-  `route_name` varchar(64) NOT NULL DEFAULT 'default',
-  `directory` varchar(64) DEFAULT NULL,
-  `controller` varchar(64) DEFAULT NULL,
-  `action` varchar(64) DEFAULT NULL,
-  `object_id` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_resource_parent` (`parent_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
--- If your `roles` table looks different - try to merge them or drop yours and use this one
+-- -----------------------------------------------------
+-- Table `roles`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `roles` (
+  `id` TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `parent_id` TINYINT(3) UNSIGNED NULL DEFAULT NULL ,
+  `name` VARCHAR(32) NOT NULL ,
+  `description` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `uniq_name` (`name` ASC) ,
+  INDEX `fk_role_parent` (`parent_id` ASC) ,
+  CONSTRAINT `fk_role_parent`
+    FOREIGN KEY (`parent_id` )
+    REFERENCES `roles` (`id` )
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
 
 
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-  `parent_id` tinyint(3) unsigned DEFAULT NULL,
-  `name` varchar(32) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_name` (`name`),
-  KEY `fk_role_parent` (`parent_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `resources`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `resources` (
+  `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `parent_id` SMALLINT(5) UNSIGNED NULL DEFAULT NULL ,
+  `route_name` VARCHAR(64) NOT NULL DEFAULT 'default' ,
+  `directory` VARCHAR(64) NULL DEFAULT NULL ,
+  `controller` VARCHAR(64) NULL DEFAULT NULL ,
+  `action` VARCHAR(64) NULL DEFAULT NULL ,
+  `params` VARCHAR(256) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_resource_parent` (`parent_id` ASC) ,
+  CONSTRAINT `fk_resource_parent`
+    FOREIGN KEY (`parent_id` )
+    REFERENCES `resources` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
 
 
-ALTER TABLE `acls`
-  ADD CONSTRAINT `fk_acl_action` FOREIGN KEY (`action_id`) REFERENCES `actions` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_acl_resource` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_acl_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE;
+-- -----------------------------------------------------
+-- Table `actions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `actions` (
+  `id` TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(20) NOT NULL ,
+  `score` SMALLINT(2) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
 
-ALTER TABLE `resources`
-  ADD CONSTRAINT `fk_resource_parent` FOREIGN KEY (`parent_id`) REFERENCES `resources` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `roles`
-  ADD CONSTRAINT `fk_role_parent` FOREIGN KEY (`parent_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE;
+-- -----------------------------------------------------
+-- Table `acls`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `acls` (
+  `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `role_id` TINYINT(3) UNSIGNED NOT NULL ,
+  `resource_id` SMALLINT(5) UNSIGNED NOT NULL ,
+  `action_id` TINYINT(3) UNSIGNED NOT NULL ,
+  `regulation` ENUM('allow','deny') NOT NULL DEFAULT 'allow' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_acl_role` (`role_id` ASC) ,
+  INDEX `fk_acl_resource` (`resource_id` ASC) ,
+  INDEX `fk_acl_action` (`action_id` ASC) ,
+  CONSTRAINT `fk_acl_action`
+    FOREIGN KEY (`action_id` )
+    REFERENCES `actions` (`id` )
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_acl_resource`
+    FOREIGN KEY (`resource_id` )
+    REFERENCES `resources` (`id` )
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_acl_role`
+    FOREIGN KEY (`role_id` )
+    REFERENCES `roles` (`id` )
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
 
-COMMIT;
+
+-- -----------------------------------------------------
+-- Table `assertion_types`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `assertion_types` (
+  `id` SMALLINT(2) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `assertions`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `assertions` (
+  `id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(64) NOT NULL ,
+  `acl_id` SMALLINT(5) UNSIGNED NOT NULL ,
+  `params` VARCHAR(256) NULL DEFAULT NULL ,
+  `condition` VARCHAR(45) NULL ,
+  `type_id` SMALLINT(2) UNSIGNED NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_assertion_type` (`type_id` ASC) ,
+  INDEX `fk_assertion_acl` (`acl_id` ASC) ,
+  CONSTRAINT `fk_assertion_type`
+    FOREIGN KEY (`type_id` )
+    REFERENCES `assertion_types` (`id` )
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_assertion_acl`
+    FOREIGN KEY (`acl_id` )
+    REFERENCES `acls` (`id` )
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
